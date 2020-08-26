8a84eb17ac60621c4d9c543e1bf1ffe04294b938
deshiteam <deshiteam24@gmail.com>
2020-08-27 01:01:48 +0600

    assignent initial setup

---
+++ fakeData/courses.js
@@ -0,0 +1,43 @@
+var courses =
+    [{"key":"MIRTECHPRO01","category":"graphic Design","name":"Adobe Photoshop Fundamentals","mentor":"Shohag Mir","sit":50,"img":"https://images-na.ssl-images-amazon.com/images/I/51VCP05020L._AC_US218_.jpg","price":57.99,"admission":3.99},
+
+    {"key":"MIRTECHPRO01","category":"graphic Design","name":"Adobe Photoshop Fundamentals","mentor":"Shohag Mir","sit":50,"img":"https://images-na.ssl-images-amazon.com/images/I/51VCP05020L._AC_US218_.jpg","price":57.99,"admission":3.99},
+
+    {"key":"MIRTECHPRO01","category":"graphic Design","name":"Adobe Photoshop Fundamentals","mentor":"Shohag Mir","sit":50,"img":"https://images-na.ssl-images-amazon.com/images/I/51VCP05020L._AC_US218_.jpg","price":57.99,"admission":3.99},
+
+    {"key":"MIRTECHPRO01","category":"graphic Design","name":"Adobe Photoshop Fundamentals","mentor":"Shohag Mir","sit":50,"img":"https://images-na.ssl-images-amazon.com/images/I/51VCP05020L._AC_US218_.jpg","price":57.99,"admission":3.99},
+
+    {"key":"MIRTECHPRO01","category":"graphic Design","name":"Adobe Photoshop Fundamentals","mentor":"Shohag Mir","sit":50,"img":"https://images-na.ssl-images-amazon.com/images/I/51VCP05020L._AC_US218_.jpg","price":57.99,"admission":3.99},
+
+    {"key":"MIRTECHPRO01","category":"graphic Design","name":"Adobe Photoshop Fundamentals","mentor":"Shohag Mir","sit":50,"img":"https://images-na.ssl-images-amazon.com/images/I/51VCP05020L._AC_US218_.jpg","price":57.99,"admission":3.99},
+
+    {"key":"MIRTECHPRO01","category":"graphic Design","name":"Adobe Photoshop Fundamentals","mentor":"Shohag Mir","sit":50,"img":"https://images-na.ssl-images-amazon.com/images/I/51VCP05020L._AC_US218_.jpg","price":57.99,"admission":3.99},
+
+    {"key":"MIRTECHPRO01","category":"graphic Design","name":"Adobe Photoshop Fundamentals","mentor":"Shohag Mir","sit":50,"img":"https://images-na.ssl-images-amazon.com/images/I/51VCP05020L._AC_US218_.jpg","price":57.99,"admission":3.99},
+
+    {"key":"MIRTECHPRO01","category":"graphic Design","name":"Adobe Photoshop Fundamentals","mentor":"Shohag Mir","sit":50,"img":"https://images-na.ssl-images-amazon.com/images/I/51VCP05020L._AC_US218_.jpg","price":57.99,"admission":3.99},
+
+    {"key":"MIRTECHPRO01","category":"graphic Design","name":"Adobe Photoshop Fundamentals","mentor":"Shohag Mir","sit":50,"img":"https://images-na.ssl-images-amazon.com/images/I/51VCP05020L._AC_US218_.jpg","price":57.99,"admission":3.99},
+
+    {"key":"MIRTECHPRO01","category":"graphic Design","name":"Adobe Photoshop Fundamentals","mentor":"Shohag Mir","sit":50,"img":"https://images-na.ssl-images-amazon.com/images/I/51VCP05020L._AC_US218_.jpg","price":57.99,"admission":3.99},
+
+    {"key":"MIRTECHPRO01","category":"graphic Design","name":"Adobe Photoshop Fundamentals","mentor":"Shohag Mir","sit":50,"img":"https://images-na.ssl-images-amazon.com/images/I/51VCP05020L._AC_US218_.jpg","price":57.99,"admission":3.99},
+
+    {"key":"MIRTECHPRO01","category":"graphic Design","name":"Adobe Photoshop Fundamentals","mentor":"Shohag Mir","sit":50,"img":"https://images-na.ssl-images-amazon.com/images/I/51VCP05020L._AC_US218_.jpg","price":57.99,"admission":3.99},
+
+    {"key":"MIRTECHPRO01","category":"graphic Design","name":"Adobe Photoshop Fundamentals","mentor":"Shohag Mir","sit":50,"img":"https://images-na.ssl-images-amazon.com/images/I/51VCP05020L._AC_US218_.jpg","price":57.99,"admission":3.99},
+
+    [{"key":"MIRTECHPRO01","category":"graphic Design","name":"Adobe Photoshop Fundamentals","mentor":"Shohag Mir","sit":50,"img":"https://images-na.ssl-images-amazon.com/images/I/51VCP05020L._AC_US218_.jpg","price":57.99,"admission":3.99},
+
+    {"key":"MIRTECHPRO01","category":"graphic Design","name":"Adobe Photoshop Fundamentals","mentor":"Shohag Mir","sit":50,"img":"https://images-na.ssl-images-amazon.com/images/I/51VCP05020L._AC_US218_.jpg","price":57.99,"admission":3.99},
+
+    {"key":"MIRTECHPRO01","category":"graphic Design","name":"Adobe Photoshop Fundamentals","mentor":"Shohag Mir","sit":50,"img":"https://images-na.ssl-images-amazon.com/images/I/51VCP05020L._AC_US218_.jpg","price":57.99,"admission":3.99},
+
+    {"key":"MIRTECHPRO01","category":"graphic Design","name":"Adobe Photoshop Fundamentals","mentor":"Shohag Mir","sit":50,"img":"https://images-na.ssl-images-amazon.com/images/I/51VCP05020L._AC_US218_.jpg","price":57.99,"admission":3.99},
+
+    {"key":"MIRTECHPRO01","category":"graphic Design","name":"Adobe Photoshop Fundamentals","mentor":"Shohag Mir","sit":50,"img":"https://images-na.ssl-images-amazon.com/images/I/51VCP05020L._AC_US218_.jpg","price":57.99,"admission":3.99},
+
+    {"key":"MIRTECHPRO01","category":"graphic Design","name":"Adobe Photoshop Fundamentals","mentor":"Shohag Mir","sit":50,"img":"https://images-na.ssl-images-amazon.com/images/I/51VCP05020L._AC_US218_.jpg","price":57.99,"admission":3.99}];
+
+
+    export default courses;
